d982c1bddfce1e85a544a3abf1c596f6412a4782
Tiago Calado <tiagocalado@hotmail.it>
2020-09-12 17:26:42 +0100

    uito.py 1.1
    little changes on the comments

--- uito.py
+++ uito.py
@@ -1,10 +1,10 @@
 import os
 
-# small program to convert ui files to .py files of the qt5 Design once the "view python code" option doesn't work
+# small program to convert .ui to .py files of the qt5 Design once the "view python code" option doesn't work
 # paste this file in a python directory that is a path enviroment
 # one like "C:\Users\"your_user"\AppData\Local\Programs\Python\Python38-32\Scripts\"
-# then to use just do a "cd ui_file_directory" and call "uipy.py"
-# the file name input should not have extension.
+# then to use just do a "cd ui_file_directory" and call "uito.py"
+# the file name input should not have extension
 # for example "amazing.ui" should be inputed like "amazing"
 
 file = input("ui file name without extension >  ")
