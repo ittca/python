4abd023989d5415a4b0e3075e23ec38e3a28874f
Tiago Calado <tiagocalado@hotmail.it>
2020-09-12 18:14:29 +0100

    uito.py 1.2
    added the if statement to see if the file exist or not

--- uito.py
+++ uito.py
@@ -1,11 +1,16 @@
 import os
+from os import path
 
-# small program to convert .ui to .py files of the qt5 Design once the "view python code" option doesn't work
+# small program to convert ui files to .py files of the qt5 Design once the "view python code" option doesn't work
 # paste this file in a python directory that is a path enviroment
 # one like "C:\Users\"your_user"\AppData\Local\Programs\Python\Python38-32\Scripts\"
-# then to use just do a "cd ui_file_directory" and call "uito.py"
-# the file name input should not have extension
+# then to use just do a "cd ui_file_directory" and call "uipy.py"
+# the file name input should not have extension.
 # for example "amazing.ui" should be inputed like "amazing"
 
 file = input("ui file name without extension >  ")
-os.system(f'python -m PyQt5.uic.pyuic -x {file}.ui -o {file}.py')
+if (path.isfile(f'{file}.ui')):
+    os.system(f'python -m PyQt5.uic.pyuic -x {file}.ui -o {file}.py')
+    print(f'\n {file}.py has been created with success!')
+else:
+    print(f'\nThe {file}.ui is not present in this directory')
